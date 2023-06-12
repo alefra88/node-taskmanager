d2f7e672621587265de7c4e294b8a8197a5e6de7
alefra88 <efra.rlz@gmail.com>
2023-06-12 05:18:16 -0600

    add ../controllers/tasks

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const tasks = require("./routes/tasks.js")
+const tasks = require("./routes/tasks")
 
 
 //MIDDLEWARE
---
+++ controllers/tasks.js
@@ -0,0 +1,6 @@
+const getAllTasks = (req, res) => {
+  res.send("all items from the file");
+};
+
+module.exports = { getAllTasks };
+
