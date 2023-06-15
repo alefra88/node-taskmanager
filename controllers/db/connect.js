dadb5814fb949df794c7452b4092b5d3dd4c6933
alefra88 <efra.rlz@gmail.com>
2023-06-15 04:58:49 -0600

    taking out db from controllers

--- controllers/db/connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectionString =
-  "mongodb+srv://<alanef88>:<a4969a>@cluster0.3narixn.mongodb.net/taskDB?retryWrites=true&w=majority";
-
-mongoose.connect(connectionString);
