8c9c9e288a568c31ad49e56a5945297b8ed92a53
alefra88 <efra.rlz@gmail.com>
2023-06-15 05:46:46 -0600

    refactoring code

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,13 @@ const mongoose = require("mongoose");
 const connectionString =
   "mongodb+srv://alanef88:a4969a@cluster0.3narixn.mongodb.net/taskDB?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("CONNECTED TO DATABASE..."))
-  .catch((err) => console.log(err));
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = connectDB;
