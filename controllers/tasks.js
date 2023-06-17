8a5a3b4c7ecb77b52bc4fc9f5ad231b9a2201656
alefra88 <efra.rlz@gmail.com>
2023-06-17 06:46:15 -0600

    ending CRUD

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,13 +31,34 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("update item from the file");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      any;
+      return res.status(404).json({ msg: `No task with ID : ${taskID}` });
+    }
+    // res.status(200).json({ task });
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete item from the file");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
+
 module.exports = {
   getAllTasks,
   createTask,
